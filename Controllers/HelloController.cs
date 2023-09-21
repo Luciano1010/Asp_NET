46998511ff29394bd6c55bbe4a204611cf41319e
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-09-21 17:15:27 -0300

    HelloWord Asp

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace HelloWord.Controllers
+{
+    [Route("~/")] // estou dizendo que essa classe controladora vai trabalhar na raiz do servidor
+    [ApiController] // é uma classe que vai mostrar o caminho 
+    public class HelloController
+    {
+        [HttpGet] // metodo que vai devolver o texto que no caso é o hello word
+        public string Hello() // metodo de texto
+        {
+            return " Hello Word! Turma .NET 03 ";
+        }
+        [HttpGet ("/meunome")] // o modo de usar o mesmo endereço ip httpget("/") conhecido com endpoints , para quebrar linha <br >
+        public string MeuNome() 
+        {
+            return " Luciano Simões ";
+        }
+
+    }
+}
+
+       
+    
